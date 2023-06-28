924f281dcbd10f75430038f4428d70ce6ae7b291
compile-run-ali <aabdullah.bscs20seecs@seecs.edu.pk>
2023-06-28 15:50:44 +0500

    added banners from backend

--- src/components/HomeLayout/Slider/Slider.js
+++ src/components/HomeLayout/Slider/Slider.js
@@ -1,70 +1,56 @@
-import React from 'react'
-import { CarouselProvider, Slider, Slide } from 'pure-react-carousel'
-import 'pure-react-carousel/dist/react-carousel.es.css'
-import SlideContent from './SlideContent'
-
-const slide_images_mobile = [
-  '/images/slider/banner1.jpeg',
-  '/images/slider/banner2.jpeg',
-  '/images/slider/banner3.jpeg',
-
-  '/images/slider/banner1.jpeg',
-  '/images/slider/banner2.jpeg',
-  '/images/slider/banner3.jpeg',
-  '/images/slider/banner1.jpeg',
-  '/images/slider/banner2.jpeg',
-  '/images/slider/banner3.jpeg',
-]
-
-const slide_images = [
-  {
-    img1: '/images/slider/banner1.jpeg',
-    img2: '/images/slider/banner2.jpeg',
-    img3: '/images/slider/banner3.jpeg',
-  },
-  {
-    img1: '/images/slider/banner1.jpeg',
-    img2: '/images/slider/banner2.jpeg',
-    img3: '/images/slider/banner3.jpeg',
-  },
-  {
-    img1: '/images/slider/banner1.jpeg',
-    img2: '/images/slider/banner2.jpeg',
-    img3: '/images/slider/banner3.jpeg',
-  },
-]
+import React, { useState, useEffect } from 'react';
+import { CarouselProvider, Slider, Slide } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import SlideContent from './SlideContent';
+import axios from 'axios';
+import { BACKEND_URL } from 'config/constants/LaunchpadAddress';
 
 export default function Carousel({ mobileView }) {
+  const [banners, setBanners] = useState([]);
+
+  useEffect(() => {
+    const fetchBanners = async () => {
+      try {
+        const response = await axios.get(`${BACKEND_URL}/api/banner`); // Replace '/api/banners' with your actual API endpoint
+        setBanners(response.data);
+      } catch (error) {
+        console.log('Error fetching banners:', error);
+      }
+    };
+
+    fetchBanners();
+  }, []);
+
   if (mobileView) {
     return (
       <CarouselProvider
         naturalSlideWidth={70}
         naturalSlideHeight={40}
-        totalSlides={9}
+        totalSlides={banners.length}
         isPlaying={true}
         interval={3000}
         visibleSlides={1.3}
       >
         <Slider>
-          {slide_images_mobile.map((slide, index) => (
+          {banners.map((banner, index) => (
             <Slide key={index} index={index}>
-              <SlideContent img1={slide} mobile />
+              <SlideContent img1={banner.url} mobile />
             </Slide>
           ))}
         </Slider>
       </CarouselProvider>
-    )
+    );
   }
 
   return (
-    <CarouselProvider naturalSlideWidth={70} naturalSlideHeight={15} totalSlides={3} isPlaying={true} interval={3000}>
+    <CarouselProvider naturalSlideWidth={70} naturalSlideHeight={15} totalSlides={banners.length} isPlaying={true} interval={3000}>
       <Slider>
-        {slide_images.map((slide, index) => (
+        {banners.map((banner, index) => (
           <Slide key={index} index={index}>
-            <SlideContent img1={slide.img1} img2={slide.img2} img3={slide.img3} />
+            <SlideContent img1={banner.url} img2={banner.url2} img3={banner.url3} />
           </Slide>
         ))}
       </Slider>
     </CarouselProvider>
-  )
+  );
 }
