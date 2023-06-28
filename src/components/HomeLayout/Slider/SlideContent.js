9bf1183f66e53cc24a2ecc06ee624d57e891c043
compile-run-ali <aabdullah.bscs20seecs@seecs.edu.pk>
2023-06-28 17:12:24 +0500

    get images from backend

--- src/components/HomeLayout/Slider/SlideContent.js
+++ src/components/HomeLayout/Slider/SlideContent.js
@@ -1,26 +1,16 @@
 import React from 'react'
 
-export default function SlideContent({ img1, img2, img3, mobile }) {
+export default function SlideContent({name, img, mobile }) {
   if (mobile) {
     return (
       <div className="w-full px-2">
-        <img className="w-full" src={img1} alt="banner-1" />
+        <img className="w-full" src={img} alt={name}/>
       </div>
     )
   }
   return (
-    <div className=" flex gap-5">
       <div className="w-1/3 pr-2">
-        <img className="w-full rounded-xl" src={img1} alt="banner-1" />
+        <img className="w-full rounded-xl" src={img} alt={name} />
       </div>
-
-      <div className="w-1/3 flex justify-end pr-2">
-        <img className="w-full rounded-xl" src={img2} alt="banner-2" />
-      </div>
-
-      <div className="w-1/3 flex justify-end pr-2">
-        <img className="w-full rounded-xl" src={img3} alt="banner-3" />
-      </div>
-    </div>
   )
 }
--- src/components/HomeLayout/Slider/Slider.js
+++ src/components/HomeLayout/Slider/Slider.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { CarouselProvider, Slider, Slide } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
-import SlideContent from './SlideContent';
-import axios from 'axios';
-import { BACKEND_URL } from 'config/constants/LaunchpadAddress';
+import React, { useState, useEffect } from "react";
+import { CarouselProvider, Slider, Slide } from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import SlideContent from "./SlideContent";
+import axios from "axios";
+import { BACKEND_URL } from "config/constants/LaunchpadAddress";
 
 export default function Carousel({ mobileView }) {
   const [banners, setBanners] = useState([]);
@@ -14,7 +14,7 @@ export default function Carousel({ mobileView }) {
         const response = await axios.get(`${BACKEND_URL}/api/banner`); // Replace '/api/banners' with your actual API endpoint
         setBanners(response.data);
       } catch (error) {
-        console.log('Error fetching banners:', error);
+        console.log("Error fetching banners:", error);
       }
     };
 
@@ -34,7 +34,7 @@ export default function Carousel({ mobileView }) {
         <Slider>
           {banners.map((banner, index) => (
             <Slide key={index} index={index}>
-              <SlideContent img1={banner.url} mobile />
+              <SlideContent img={banner.url} mobile name = {banner.name}/>
             </Slide>
           ))}
         </Slider>
@@ -43,13 +43,27 @@ export default function Carousel({ mobileView }) {
   }
 
   return (
-    <CarouselProvider naturalSlideWidth={70} naturalSlideHeight={15} totalSlides={banners.length} isPlaying={true} interval={3000}>
+    <CarouselProvider
+      naturalSlideWidth={70}
+      naturalSlideHeight={15}
+      totalSlides={Math.ceil(banners.length / 3)} // Total slides based on the number of banner groups
+      isPlaying={true}
+      interval={3000}
+    >
       <Slider>
-        {banners.map((banner, index) => (
-          <Slide key={index} index={index}>
-            <SlideContent img1={banner.url} img2={banner.url2} img3={banner.url3} />
-          </Slide>
-        ))}
+        {Array.from({ length: Math.ceil(banners.length / 3) }).map(
+          (_, slideIndex) => (
+            <Slide key={slideIndex} index={slideIndex}>
+              <div className="banner-group gap-5 flex">
+                {banners
+                  .slice(slideIndex * 3, slideIndex * 3 + 3)
+                  .map((banner, bannerIndex) => (
+                    <SlideContent key={bannerIndex} img={banner.url} name={banner.name} />
+                  ))}
+              </div>
+            </Slide>
+          )
+        )}
       </Slider>
     </CarouselProvider>
   );
